cf6755300769710158ae570269e744bf8ff49388
GDRMVRM <gdrmvrm@gmail.com>
2022-02-22 16:24:51 +0000

    Add release_bike method & test

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,10 +1,16 @@
 require 'boris_bikes'
 
 describe DockingStation do
-  describe "#encode" do
-    it "encodes text with the specified key" do
-      expect(subject.encode(text, "secretkey")). to eq encoded_text
-    end
+  # describe "#encode" do
+  #   it "encodes text with the specified key" do
+  #     expect(subject.encode(text, "secretkey")). to eq encoded_text
+  #   end
+  # end
+
+  describe "#release_bike" do 
+    it "calls release_bike on docking station" do
+      expect(subject.respond_to?(:release_bike))
+   end
   end
 
   
