37574bf17206abfa77068a71b841071e33d09cfe
Paul <pkilgarriff@gmail.com>
2022-02-23 17:06:48 +0000

    Add docking_station test - return bike object when requested
    Co-authored-by: Gayathri Dharmavaram <gdrmvrm@users.noreply.github.com>

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,8 @@ describe DockingStation do
   it "responds to release_bike" do
     expect(subject.respond_to?(:release_bike)).to eq true
   end
+
+  it "returns a Bike when requested" do
+    expect(subject.release_bike.instance_of?(Bike)).to eq true 
+  end
 end
