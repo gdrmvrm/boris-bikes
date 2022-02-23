08b58fd57ba2c5d5337d5b18c7cd5b9445e263d5
Paul <pkilgarriff@gmail.com>
2022-02-23 17:06:10 +0000

    Add feature_spec.rb add tests - releasing working bikes - storing bikes when docked
    Co-authored-by: Gayathri Dharmavaram <gdrmvrm@users.noreply.github.com>

---
+++ spec/feature_spec.rb
@@ -0,0 +1,17 @@
+require 'boris_bikes'
+
+describe 'using a docking station' do
+  it 'releases an actual bike that I can check works' do
+    station = DockingStation.new
+    bike = station.release_bike
+    expect(bike.working?).to eq true
+  end
+
+  it 'stores a bike when I dock it' do
+    station = DockingStation.new
+    bike = Bike.new
+    station.dock(bike)
+    expect(station.bikes[0].instance_of?(Bike)).to eq true
+  end
+
+end
