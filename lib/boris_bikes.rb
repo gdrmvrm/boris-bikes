262488b4fcbfd1fc58895734cfa882e4fddf2a9f
Paul <pkilgarriff@gmail.com>
2022-02-23 21:36:09 +0000

    DockingStation - add dock_bike method - add explicit initialize - add attr_reader for @bikes
    Co-authored-by: Gayathri Dharmavaram <gdrmvrm@users.noreply.github.com>

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,6 +1,18 @@
 class DockingStation
+  attr_reader :bikes
+
+  def initialize
+    @bikes = [Bike.new]
+  end
+
   def release_bike
     return Bike.new
+  end
+
+  def dock_bike(cupcake)
+    p cupcake
+    cupcake = "Bonjour"
+    p cupcake
   end
 end
 
