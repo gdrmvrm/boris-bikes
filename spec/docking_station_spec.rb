380916ec389d2beb83bbf6097d9c9f1ced55bc4a
Paul <pkilgarriff@gmail.com>
2022-02-23 21:37:57 +0000

    Add DockingStation tests  - responds to dock_bike  - responds to bikes  - returns array of bikes  - spec for checking bike return
    Co-authored-by: Gayathri Dharmavaram <gdrmvrm@users.noreply.github.com>

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,26 @@ describe DockingStation do
     expect(subject.respond_to?(:release_bike)).to eq true
   end
 
+  it 'responds to dock_bike' do
+    expect(subject.respond_to?(:dock_bike)).to eq true
+  end
+
+  it 'responds to bikes' do
+    expect(subject.respond_to?(:bikes)).to eq true
+  end
+
+  it "returns an array of bikes when bikes called" do
+    expect(subject.bikes.instance_of?(Array)).to eq true 
+  end
+
   it "returns a Bike when requested" do
     expect(subject.release_bike.instance_of?(Bike)).to eq true 
+  end
+
+  it 'releases the same bike that has just been docked' do
+    # we put in a bike
+    # we have some way of identifying that bike ?object id
+    # we ask for a bike 
+    # we check if it is the same bike (use identifier from above)
   end
 end
